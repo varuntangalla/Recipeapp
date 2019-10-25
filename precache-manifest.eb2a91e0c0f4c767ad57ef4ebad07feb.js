82c8e426372649de13ae35806e70634db1f39edc
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-25 14:11:23 -0700

    Updates

---
+++ precache-manifest.eb2a91e0c0f4c767ad57ef4ebad07feb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f187e68be4f1eedc9297756b0478377f",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "a7db0033836831fe72f4",
+    "url": "/recipeapp/static/css/main.6ea5ee9f.chunk.css"
+  },
+  {
+    "revision": "78ca27add78aff03a978",
+    "url": "/recipeapp/static/js/2.f46de22c.chunk.js"
+  },
+  {
+    "revision": "a7db0033836831fe72f4",
+    "url": "/recipeapp/static/js/main.f2a2302f.chunk.js"
+  },
+  {
+    "revision": "8cc7b0c8132ad4327e79",
+    "url": "/recipeapp/static/js/runtime-main.19b4305c.js"
+  }
+]);
